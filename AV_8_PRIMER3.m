15aa207e8dd5a73f52d50fa5c43044d2ec39e3a6
Despina-Mojsova <mojsova_despina@yahoo.com>
2025-05-26 15:52:44 +0200

    Add files via upload
    Оди со моделот во симулинк PRIMER3

---
+++ AV_8_PRIMER3.m
@@ -0,0 +1,15 @@
+%% tuka pravam definiranje na parametrite q0,q1,q2,T0 za da mozam da gi menuvam ottuka
+clc;
+clear;
+% vreme na zemanje odbiroci
+T0=1;
+q0=1.5505;
+q1=-2.1671;
+q2=0.6781;
+sim('PRIMER3')
+% moze i od command Window, pobrzo e kaj mene 
+%T0=1;
+%q0=1.5505;
+%q1=-2.1671;
+%q2=0.6781;
+
